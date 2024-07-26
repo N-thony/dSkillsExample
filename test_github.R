804655daa3a13b5721cf450331e0e69b3dc58b3e
Ntalumeso <antoine.ntalumeso@idems.international>
2024-07-26 17:06:19 +0300

    small change

--- test_github.R
+++ test_github.R
@@ -4,7 +4,7 @@ library(datasets)  # For iris dataset
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Iris Dataset Scatter Plotofr WP3 Training in DRC"),
+  titlePanel("Iris Dataset Scatter Plotofr"),
   
   # Sidebar layout with input and output definitions
   sidebarLayout(
