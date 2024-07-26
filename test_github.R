76c937e9825b0caa20e5da6b37b61cc38614f35f
B-Moise <moisebizimana2017@gmail.com>
2024-07-26 16:54:54 +0200

    Changes at the TitlePanel

--- test_github.R
+++ test_github.R
@@ -4,7 +4,7 @@ library(datasets)  # For iris dataset
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Iris Dataset Scatter Plotofr"),
+  titlePanel("Iris Dataset Scatter Plot for training in DRC"),
   
   # Sidebar layout with input and output definitions
   sidebarLayout(
